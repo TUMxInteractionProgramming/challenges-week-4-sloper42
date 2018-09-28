1673266a9ca65882d0801732bdc245dee24c5d74
Robert Wilhelm <robert.wilhelm@gmx.net>
2018-09-29 00:22:40 +0200

    Challenge 8.3

--- js/channels.js
+++ js/channels.js
@@ -2,7 +2,7 @@
 var currentChannel;
 
 var Yummy = {
-   name: "#Yummy",
+   name: "Yummy",
    createdOn:Date(2016,4,1),
    createdBy:"minus.plus.yummy",
    starred: false,
@@ -11,7 +11,7 @@ var Yummy = {
 };
 
 var SevenContinents = {
-    name: "#SevenContinents",
+    name: "SevenContinents",
     createdOn:Date(2016,4,1),
     createdBy:"cheeses.yard.applies",
     starred: true,
@@ -20,7 +20,7 @@ var SevenContinents = {
  };
 
  var KillerApp = {
-    name: "#KillerApp",
+    name: "KillerApp",
     createdOn:Date(2016,4,1),
     createdBy:"minus.plus.yummy",
     starred: false,
@@ -29,7 +29,7 @@ var SevenContinents = {
  };
 
  var FirstPersonOnMars = {
-    name: "#FirstPersonOnMars",
+    name: "FirstPersonOnMars",
     createdOn:Date(2016,4,1),
     createdBy:"snippet.atom.grid",
     starred: true,
@@ -38,10 +38,42 @@ var SevenContinents = {
  };
 
  var Octoberfest = {
-    name: "#Octoberfest",
+    name: "Octoberfest",
     createdOn:Date(2016,4,1),
     createdBy:"minus.plus.yummy",
     starred: false,
     expiresIn: 100,
     messageCount: 999
  };
+
+ function listChannels() {
+   
+    var tmp;
+    tmp = createChannelElement(Yummy);
+  
+    $('#channel-list').append(tmp);
+    tmp = createChannelElement(SevenContinents);
+    $('#channel-list').append(tmp);
+    tmp = createChannelElement(KillerApp);
+    $('#channel-list').append(tmp);
+    tmp = createChannelElement(FirstPersonOnMars);
+    $('#channel-list').append(tmp);
+    tmp = createChannelElement(Octoberfest);
+    $('#channel-list').append(tmp);
+
+    console.log($('#channel-list').html());
+ }
+
+ function createChannelElement(channelObject) {
+
+    var star = channelObject.starred ? "fas" : "far";
+
+    var ret = $('<li>').attr('onclick','switchChannel(' + channelObject.name + ')').text(
+        '#' +channelObject.name).append($('<span>').attr('style','font-size: 24px').addClass('channel-meta').append(
+            $('<i>').addClass(star +' fa-star'), $('<i>').addClass('fas fa-chevron-right')
+        )
+        );
+
+    return ret;
+ }
+ 
